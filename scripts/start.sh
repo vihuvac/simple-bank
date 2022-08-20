7d04593f55800f17562b9674f08b88ffad154ddf
Víctor Hugo Valle Castillo <victouk@gmail.com>
2022-08-20 00:23:20 -0300

    Refresh environment variables before running database migrations.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "---> Refresh environment variables before running database migrations:"
+source /app/app.env
+
 echo "---> Run the database migrations:"
 /app/migrate -path /app/migrations -database "$DB_URI" -verbose up
 
